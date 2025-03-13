225bd6a68ad2f39eb4484fd08c654453ad310eb1
Julio Rubio <juliorubio6767@gmail.com>
2025-03-13 10:33:33 -0500

    ya no me gusto modificando cambios  func.py

--- func.py
+++ func.py
@@ -8,6 +8,4 @@ valores = []
 for i in range(cantidad):
     digito = int(input(f"Ingrese el número {i + 1}: "))
     valores.append(digito)  # ✅ Agregar el número ingresado
-
-print("Resultado de la suma:")
 print(f"Al sumar los {cantidad} numeros nos da como resultado:", sumar(*valores))
